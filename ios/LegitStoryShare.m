b2bb344c02fe14b4c0e544386d85e357df06f8aa
Alex Wait <alex@chirpify.com>
2019-10-17 13:23:50 -0700

    Adding more required files (bridge header mostly)

--- ios/LegitStoryShare.m
+++ ios/LegitStoryShare.m
@@ -1,14 +1,43 @@
 #import "LegitStoryShare.h"
+#import <React/RCTLog.h>
 
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
 
-@implementation LegitStoryShare
+@interface RCT_EXTERN_MODULE(RNLegitStoryShare, NSObject)
 
-RCT_EXPORT_MODULE()
+RCT_EXTERN_METHOD(addEvent)
 
-RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
-{
-    // TODO: Implement some actually useful functionality
-	callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
-}
+RCT_EXTERN_METHOD(isInstagramAvailable: (RCTPromiseResolveBlock)resolve
+                  rejecter: (RCTPromiseRejectBlock)reject)
 
 @end
+
+//@implementation LegitStoryShare
+//
+//RCT_EXPORT_MODULE()
+//
+//// RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
+//// {
+////     // TODO: Implement some actually useful functionality
+//// 	callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+//// }
+//
+//RCT_EXPORT_METHOD(addEvent:(NSString *)name location:(NSString *)location)
+//{
+//  RCTLogInfo(@"final test Pretending to create an event %@ at %@", name, location);
+//}
+//
+//@end
+//
+//
+//
+//// CalendarManagerBridge.m
+//#import <React/RCTBridgeModule.h>
+//
+//@interface RCT_EXTERN_MODULE(CalendarManager, NSObject)
+//
+//RCT_EXTERN_METHOD(addEvent:(NSString *)name location:(NSString *)location )
+//
+//@end
+
--- react-native-legit-story-share/index.js
+++
@@ -1,5 +0,0 @@
-import { NativeModules } from 'react-native';
-
-const { LegitStoryShare } = NativeModules;
-
-export default LegitStoryShare;
